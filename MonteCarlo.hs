fc3540585711a7d378e64de89fa18aae4c4ee4dd
Cale Sennett <calesennett@gmail.com>
2014-11-22 11:42:30 -0600

    finish Monte Carlo simulation

--- MonteCarlo.hs
+++ MonteCarlo.hs
@@ -1,32 +1,102 @@
 module MonteCarlo
-    (
+    ( genPricePath
+    , pricePaths
+    , deltaT
+    , payoffCall
+    , payoffPut
+    , Option (..)
     ) where
 
 import           Data.Random.Normal
+import           Math.Statistics
 
 data Option = Option { optionType :: String
                      , stock      :: Double
                      , strike     :: Double
-                     , riskFree   :: Double
                      , time       :: Double
                      , vol        :: Double
+                     , drift      :: Double
                      }
+deltaT
+    :: Option
+    -> Double
+deltaT option = (time option) / 365
+
+main =  do
+        let option  = Option { optionType="call"
+                             , stock=31.25
+                             , strike=22.75
+                             , time=1.0
+                             , vol=0.5
+                             , drift=0.1
+                             }
+            trials  = 10000
+        allPaths <- sequence $ pricePaths option (stock option) trials
+        let averages = map mean allPaths
+            payoff  = mean $ calcPayoffs option averages
+            discounted = payoff / (1.0731)**(time option)
+        print discounted
+
+calcPayoffs
+    :: Option
+    -> [Double]
+    -> [Double]
+calcPayoffs
+    option
+    averages
+    | optionType o == "call" = map (payoffCall $ strike o) averages
+    | otherwise              = map (payoffPut $ strike o) averages
+        where o = option
+
+pricePaths
+    :: Option
+    -> Double
+    -> Double
+    -> [IO [Double]]
+pricePaths
+    option
+    stock
+    trials
+    | trials == 0 = []
+    | otherwise   = do
+                    path <- [(genPricePath option stock ((deltaT option) * 365^2))]
+                    paths <- [path] ++ (pricePaths option stock (trials - 1))
+                    return paths
+
+genPricePath
+    :: Option
+    -> Double
+    -> Double
+    -> IO [Double]
+genPricePath
+    option
+    stock
+    points
+    | points <= 0 = return []
+    | otherwise   = do
+                    e <- epsilon
+                    nextStep <- genPricePath option (nextPrice option stock e) (points - 1)
+                    let path = (stock:nextStep)
+                    return path
+
+nextPrice option stock e = stock + (drift o) * stock * (deltaT o) + (vol o) * stock * e * sqrt (deltaT o)
+                           where o = option
 
 payoffCall
     :: Double
     -> Double
     -> Double
 payoffCall
-    stock
-    strike = maximum [stock - strike, 0]
+    strike
+    stock = maximum [stock - strike, 0]
 
 payoffPut
     :: Double
     -> Double
     -> Double
 payoffPut
-    stock
-    strike = maximum [strike - stock, 0]
+    strike
+    stock = maximum [strike - stock, 0]
 
 epsilon :: IO Double
 epsilon =   do
---
+++ Tests.hs
@@ -0,0 +1,76 @@
+module Test.MonteCarlo where
+
+import           MonteCarlo
+import           Test.Hspec
+
+main =  hspec $ do
+        describe "deltaT" $ do
+            it "returns correct time step" $ do
+                let o = Option { optionType="call"
+                               , stock=31.25
+                               , strike=22.75
+                               , time=1.0
+                               , vol=0.5
+                               , drift=0.1
+                               }
+                deltaT o `shouldSatisfy` within 0.001 0.00273972602
+
+        describe "payoffCall" $ do
+            it "returns correct call payout" $ do
+                let stock  = 31.25
+                    strike = 29.00
+                payoffCall stock strike `shouldBe` 2.25
+
+            it "computes worthless payoff" $ do
+                let stock  = 31.25
+                    strike = 35.00
+                payoffCall stock strike `shouldBe` 0.0
+
+        describe "payoffPut" $ do
+            it "returns correct put payout" $ do
+                let stock  = 31.25
+                    strike = 35.00
+                payoffPut stock strike `shouldBe` 3.75
+
+            it "computes worthless payoff" $ do
+                let stock  = 31.25
+                    strike = 30.00
+                payoffPut stock strike `shouldBe` 0.0
+
+        describe "genPricePath" $ do
+            it "computes correct number of steps" $ do
+                let o = Option { optionType="call"
+                               , stock=31.25
+                               , strike=22.75
+                               , time=0.49315068493
+                               , vol=0.5
+                               , drift=0.1
+                               }
+                path <- genPricePath o (stock o) ((deltaT o) * 365^2)
+                length path `shouldBe` 180
+
+        describe "pricePaths" $ do
+            it "returns the correct number of trials" $ do
+                let o = Option { optionType="call"
+                               , stock=31.25
+                               , strike=22.75
+                               , time=1.0
+                               , vol=0.5
+                               , drift=0.1
+                               }
+                    trials = 10000
+                length (pricePaths o (stock o) trials) `shouldBe` 10000
+within
+    :: Double
+    -> Double
+    -> Double
+    -> Bool
+within
+    tolerance
+    expected
+    actual = if lower <= actual && actual <= upper
+             then True
+             else False
+             where lower = (expected - expected * tolerance)
+                   upper = (expected + expected * tolerance)
+
